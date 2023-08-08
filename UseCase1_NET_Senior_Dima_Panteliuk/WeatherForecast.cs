bf3c4d396a2b895a207d24abca15b56d1bc822e7
Dmytro Panteliuk <dmytro.panteliuk01@currys.co.uk>
2023-08-08 19:02:44 +0300

    UC#1 Infrastructure setup

---
+++ UseCase1_NET_Senior_Dima_Panteliuk/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace UseCase1_NET_Senior_Dima_Panteliuk;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
